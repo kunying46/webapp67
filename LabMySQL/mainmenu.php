9b3d8bcc4ce310789d13210e686c791302d63934
kunying46 <142193233+kunying46@users.noreply.github.com>
2024-09-22 23:50:35 +0700

    Add files via upload

---
+++ LabMySQL/mainmenu.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>สมาชิก</h1><br>  //create สมาชิก table
+    <?php
+    require 'conn.php';
+    $sql = "SELECT * FROM member";
+    $result = $conn->query($sql);
+    if(!$result){
+        die("Error : ". $conn->$conn_error);
+    }
+    ?>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>ชื่อ-นามสกุล</th>
+                    <th>ที่อยู่</th>
+                    <th>เบอร์โทร</th>
+                    <th> Edit </th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php // show data by fetch from database
+                    if ($result->num_rows > 0) {
+                        while($row = $result->fetch_assoc()) {
+                            echo"<tr><td>".$row["mbID"]."</td>"."<td>".$row["mbname"]." ".$row["mblastname"]."</td>"."<td>".$row["address"]."</td>"."<td>".$row["telephone"]."</td>"."<td>"."<a href='editbio.php?mbID=".$row["mbID"]."'><button> Edit </button></a>"."</td>";
+                            echo "</tr>"; 
+                            
+                        }
+                        
+                    }else {
+                        echo "0 results";
+                    }
+                    $conn->close();
+                ?>
+            </tbody>
+        </table>  //close table
+        <br>
+        <a href='insertbio.php'><button> Insert </button></a>
+        <a href='mainmenu.php'> <button> Home </button></a>
+
+</body>
+</html>
